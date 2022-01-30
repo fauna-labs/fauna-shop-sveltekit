1f899b0a7df59bcb3ad911a9c7143e1fd6364fdc
shadid12 <shadidhaquee2012@gmail.com>
2022-01-30 16:35:16 -0500

    2.a final code

---
+++ src/client.js
@@ -0,0 +1,21 @@
+import { createClient } from '@urql/svelte';
+
+export default createClient({
+  
+  /**
+    Uncomment the appropriate line according to the
+    region group where you created your database.
+  **/
+
+	//url: https://graphql.eu.fauna.com/graphql
+  url: `https://graphql.us.fauna.com/graphql`,
+
+  // url: `https://graphql.fauna.com/graphql`,
+
+  fetchOptions: () => {
+    const token = import.meta.env.VITE_PUBLIC_FAUNA_KEY;
+    return {
+      headers: { authorization: token ? `Bearer ${token}` : '' },
+    };
+  },
+}); 
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import { setClient, mutation } from '@urql/svelte';
+  import client from '../client';
+  setClient(client);
+
+  const loginMutation = mutation({
+    query: `
+      mutation ($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+          secret
+          ttl
+        }
+      }
+    `,
+  });
+  async function testLogin(e) {
+    e.preventDefault();
+    const resp = await loginMutation({ email: "test22@gmail.com", password: "test22@gmail.com" })
+    console.log(resp.data);
+  }
+</script>
+
+<div>
+  <h3>Login Test</h3>
+  <button on:click={testLogin}>Login Test</button>
+</div>
