94b119ab5734e169ca3b72c2e848f928811ae11b
shadid12 <shadidhaquee2012@gmail.com>
2022-01-30 22:09:06 -0500

    final

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,9 +1,23 @@
 <script lang="js">
   import Cookies from 'js-cookie';
+  import { userSession } from '../store.js';
+
+  let user;
+  userSession.subscribe(val => {
+    user = val;
+    const cookies = Cookies.get('fauna-session');
+    if(!val && cookies) {
+      user = JSON.parse(cookies);
+      userSession.set(user);
+    }
+  });
+
 
   function logout() {
     Cookies.remove('fauna-session');
     alert('User Logoed Out');
+    userSession.update(() => null);
+    window.location.reload();
   }
 </script>
 
@@ -15,17 +29,30 @@
   </div>
   <div class="uk-navbar-right">
     <ul class="uk-navbar-nav">
-      <li >
-        <a href='/store/new' class="uk-button uk-button-primary add-btn">
-          Add Store
-        </a>
-      </li>
-      <li >
-        <!-- svelte-ignore a11y-missing-attribute -->
-        <a on:click={logout} class="uk-button uk-button-danger add-btn">
-          Logout
-        </a>
-      </li>
+      {#if user}
+        <li >
+          <a href='/store/new' class="uk-button uk-button-primary add-btn">
+            Add Store
+          </a>
+        </li>
+        <li >
+          <!-- svelte-ignore a11y-missing-attribute -->
+          <a on:click={logout} class="uk-button uk-button-danger add-btn">
+            Logout
+          </a>
+        </li>
+      {:else}
+        <li >
+          <a href='/login' class="uk-button uk-button-primary add-btn">
+            Login
+          </a>
+        </li>
+        <li >
+          <a href='/signup' class="uk-button uk-button-danger add-btn">
+            Signup
+          </a>
+        </li>
+      {/if}
     </ul>
   </div>
 </nav>
